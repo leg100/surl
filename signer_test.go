e40ed8fc38b2329429bdcff5b6f766f7252aca65
Louis Garman <75728+leg100@users.noreply.github.com>
2022-09-29 16:12:54 +0100

    Sign query as well as path (#1)

--- signer_test.go
+++ signer_test.go
@@ -9,28 +9,50 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSignature_SignURL(t *testing.T) {
+func TestSigner(t *testing.T) {
 	sign := New([]byte("abc123"))
 
-	t.Run("signable", func(t *testing.T) {
-		u := "https://example.com/a/b/c?baz=cow&foo=bar"
-		signed, err := sign.Sign(u, time.Second*10)
-		require.NoError(t, err)
+	tests := []struct {
+		name string
+		url  string
+	}{
+		{
+			name: "with query",
+			url:  "https://example.com/a/b/c?baz=cow&foo=bar",
+		},
+		{
+			name: "without query",
+			url:  "https://example.com/a/b/c",
+		},
+		{
+			name: "with only question mark",
+			url:  "https://example.com/a/b/c?",
+		},
+		{
+			name: "only absolute path",
+			url:  "/a/b/c",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			signed, err := sign.Sign(tt.url, time.Second*10)
+			require.NoError(t, err)
 
-		// check valid URL
-		_, err = url.Parse(signed)
-		require.NoError(t, err)
+			// check valid URL
+			_, err = url.Parse(signed)
+			require.NoError(t, err)
 
-		// check valid signature
-		err = sign.Verify(signed)
-		require.NoError(t, err)
-	})
+			// check valid signature
+			err = sign.Verify(signed)
+			require.NoError(t, err)
+		})
+	}
 
 	t.Run("verifiable", func(t *testing.T) {
 		// this URL was created with a maximum lifespan:
 		// u := "https://example.com/a/b/c?baz=cow&foo=bar"
-		// signed, err := sign.SignURL(u, time.Duration(math.MaxInt64))
-		u := "https://example.com/signed/5387hhk1Dl-SCQ-BNucYgptZsJhZO4Tn-FpvHtO3j-Q.10887773970/a/b/c?baz=cow&foo=bar"
+		// signed, err := sign.Sign(u, time.Duration(math.MaxInt64))
+		u := "https://example.com/signed/_BGBJ-6OcP6GnoQz071_rU_VfMWRbi0MGLLQhfxesRg.10887835696/a/b/c?baz=cow&foo=bar"
 		err := sign.Verify(u)
 		require.NoError(t, err)
 	})
@@ -42,6 +64,11 @@ func TestSignature_SignURL(t *testing.T) {
 
 		err = sign.Verify(signed)
 		assert.Equal(t, ErrExpired, err)
+	})
+
+	t.Run("relative path", func(t *testing.T) {
+		_, err := sign.Sign("foo/bar", time.Minute)
+		assert.Error(t, err)
 	})
 
 	t.Run("invalid prefix", func(t *testing.T) {
